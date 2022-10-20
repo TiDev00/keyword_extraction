35b651c58fee83b155616eeb9b9f7fd207089edc
TiDev00 <cisse.thierno@outlook.com>
2022-10-20 02:33:07 -0400

    misc.py modif

--- utils/custom_kwx.py
+++ utils/custom_kwx.py
@@ -1,7 +1,9 @@
 """
 Line modification
 -----
-Utility to automatically modify couples of lines in package kwx/utils.py
+Utility to automatically modify couples of lines in package:
+kwx/utils.py
+past/builtins/misc.py
 Contents:
     read lines in file
     replace lines in file
@@ -10,9 +12,17 @@ Contents:
 with open('/usr/local/lib/python3.8/site-packages/kwx/utils.py', 'r', encoding='utf-8') as file:
     data = file.readlines()
 
-data[330] = "\ttexts_no_emojis = texts_no_punctuation\n"
+data[330] = "    texts_no_emojis = texts_no_punctuation\n"
 data[331] = ""
 data[332] = ""
 
 with open('/usr/local/lib/python3.8/site-packages/kwx/utils.py', 'w', encoding='utf-8') as file:
+    file.writelines(data)
+
+with open('/usr/local/lib/python3.8/site-packages/past/builtins/misc.py', 'r', encoding='utf-8') as file:
+    data = file.readlines()
+
+data[44] = "    from importlib import reload\n"
+
+with open('/usr/local/lib/python3.8/site-packages/past/builtins/misc.py', 'w', encoding='utf-8') as file:
     file.writelines(data)
